5ff67062390532bcfab3f51fcc5b881cb60f9522
BrenoVns <brenovassolersilva@gmail.com>
2022-03-31 01:57:45 -0300

    bug fixes and design

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const squares = document.querySelectorAll('.square');
-const btn = document.querySelector('button');
+const squares = document.querySelectorAll(".square");
+const btn = document.querySelector("button");
 
 let squaresSelected = 0;
 let firstSelection;
@@ -11,10 +11,10 @@ function changeSquares() {
     if (!firstSelection || !secondSelection) {
         return;
     }
-    firstSelection.className = 'square';
+    firstSelection.className = "square";
     firstSelection.className = `square square--${secondSelectionId}`;
     firstSelection.id = secondSelectionId;
-    secondSelection.className = 'square';
+    secondSelection.className = "square";
     secondSelection.className = `square square--${firstSelectionId}`;
     secondSelection.id = firstSelectionId;
     squaresSelected = 0;
@@ -22,12 +22,12 @@ function changeSquares() {
     firstSelectionId = null;
     secondSelection = null;
     secondSelectionId = null;
-    btn.classList.toggle('btn-disabled');
+    btn.classList.toggle("btn-disabled");
 }
 
 function selectSquare(square) {
     if (squaresSelected === 2) {
-        if (square.classList.contains('selected')) {
+        if (square.classList.contains("selected")) {
             if (square.id == firstSelectionId) {
                 firstSelection = null;
                 firstSelectionId = null;
@@ -36,49 +36,60 @@ function selectSquare(square) {
                 secondSelection = null;
                 secondSelectionId = null;
             }
-            square.classList.toggle('selected');
-            btn.classList.toggle('btn-disabled');
+            square.classList.toggle("selected");
+            btn.classList.toggle("btn-disabled");
             squaresSelected--;
+            console.log(squaresSelected);
             return;
         }
         return;
     }
 
-    if (square.classList.contains('selected')) {
+    if (square.classList.contains("selected")) {
         if (squaresSelected === 1) {
             firstSelection = null;
             firstSelectionId = null;
-            square.classList.toggle('selected');
+            secondSelection = null;
+            secondSelectionId = null;
+            square.classList.toggle("selected");
             squaresSelected--;
+            console.log(squaresSelected);
             return;
         }
         secondSelection = null;
         secondSelectionId = null;
-        square.classList.toggle('selected');
+        square.classList.toggle("selected");
         squaresSelected--;
+        console.log(squaresSelected);
         return;
     }
 
     if (!firstSelection) {
         firstSelection = square;
         firstSelectionId = square.id;
+        if (secondSelection) {
+            btn.classList.toggle("btn-disabled");
+        }
     } else {
         secondSelection = square;
         secondSelectionId = square.id;
-        btn.classList.toggle('btn-disabled');
+        if (firstSelection) {
+            btn.classList.toggle("btn-disabled");
+        }
     }
 
-    square.classList.toggle('selected');
+    square.classList.toggle("selected");
     squaresSelected++;
+    console.log(squaresSelected);
 }
 
 squares.forEach((square) => {
-    square.addEventListener('click', () => {
+    square.addEventListener("click", () => {
         selectSquare(square);
     });
 });
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
     if (!firstSelection && !secondSelection) {
         return;
     }
